a365437a5eb2aacaccbef006ea7297dc52aa554c
Neha Yadav <ny663922@gmail.com>
2025-07-30 00:06:52 +0530

    learning and revising js again

---
+++ validate-password/arrays/arrays.js
@@ -0,0 +1,15 @@
+console.log(typeof "neha");
+console.log(typeof 1);
+console.log(typeof null);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () { });
+
+
+
+
+
+
+
