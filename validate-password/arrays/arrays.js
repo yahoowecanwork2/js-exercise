307d0a4f889a9c58a313ae838a256307fad0ca73
Neha Yadav <ny663922@gmail.com>
2025-07-30 01:58:23 +0530

    pratice data type

--- validate-password/arrays/arrays.js
+++ validate-password/arrays/arrays.js
@@ -1,12 +1,27 @@
-console.log(typeof "neha");
-console.log(typeof 1);
-console.log(typeof null);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof {});
-console.log(typeof []);
-console.log(typeof function () { });
+// console.log(typeof "neha");
+// console.log(typeof 1);
+// console.log(typeof null);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof function () { });
 
+// Number
+
+let a = 5;
+console.log(Number(a));
+console.log(parseInt("123.33"));
+console.log(parseFloat("123.33"));
+
+// string
+let num = 5;
+console.log(String(num));
+console.log(num.toString());
+
+//boolean
+console.log(Boolean(1));
+console.log(Boolean(0));
 
 
 
