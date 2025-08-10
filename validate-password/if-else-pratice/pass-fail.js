99133b4ad16c496ff511546c1e7a6e6afa65ad72
Neha Yadav <ny663922@gmail.com>
2025-08-10 09:15:53 +0530

    more questions

--- validate-password/if-else-pratice/pass-fail.js
+++ validate-password/if-else-pratice/pass-fail.js
@@ -8,3 +8,24 @@ if ([]) {
     console.log("False");
 }
 // output:true
+
+if ({} == false) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+//output:no
+
+
+if (NaN) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+// output:answer is false
+if (null == undefined) {
+    console.log("Equal");
+} else {
+    console.log("Not equal");
+}
+// output :answer is Not equal
